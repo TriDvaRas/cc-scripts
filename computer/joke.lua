53396292e4fffc7df94bbc0a26d8b2b249f629f7
TriDvaRas <02dimon02@gmail.com>
2022-07-11 12:41:16 +0300

    joke

--- computer/joke.lua
+++ computer/joke.lua
@@ -1,5 +1,4 @@
--- ! requires json parser! pastebin get 4nRg9CHU json
-os.loadAPI("json")
+
 local monitor = peripheral.wrap("top")
 
 
@@ -13,7 +12,8 @@ local str = http.post("http://castlots.org/generator-anekdotov-online/generate.p
         ["x-requested-with"] = "XMLHttpRequest",
         ["cookie"] = "_ga=GA1.2.495309840.1600029536; _gid=GA1.2.487757344.1600029536; _ym_d=1600029536; _ym_uid=1600029536819948754; _ym_isad=1; _gat=1; _ym_visorc_29162025=w; _ym_visorc_26812653=b"
     }).readAll()
-monitor.write(str)
-local obj = json.decode(str)
+print(str)
+local obj = textutils.unserializeJSON(str)
 local value = obj.va
+print(value)
 monitor.write(value)
