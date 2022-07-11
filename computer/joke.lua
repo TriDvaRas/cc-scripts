aa2af4c5e1bf1adaefe6d2ab7892e008115e9dad
TriDvaRas <02dimon02@gmail.com>
2022-07-11 12:55:22 +0300

    Update joke.lua

--- computer/joke.lua
+++ computer/joke.lua
@@ -1,6 +1,7 @@
+-- ! requires JSON lib! wget https://raw.githubusercontent.com/TriDvaRas/cc-scripts/master/lib/JSON.lua
 
 local monitor = peripheral.wrap("top")
-
+JSON = (loadfile "JSON.lua")()
 
 local str = http.post("http://castlots.org/generator-anekdotov-online/generate.php", '{}',
     {
@@ -13,7 +14,7 @@ local str = http.post("http://castlots.org/generator-anekdotov-online/generate.p
         ["cookie"] = "_ga=GA1.2.495309840.1600029536; _gid=GA1.2.487757344.1600029536; _ym_d=1600029536; _ym_uid=1600029536819948754; _ym_isad=1; _gat=1; _ym_visorc_29162025=w; _ym_visorc_26812653=b"
     }).readAll()
 print(str)
-local obj = textutils.unserializeJSON(str)
+local obj = JSON:decode(str)
 local value = obj.va
 print(value)
 monitor.write(value)
