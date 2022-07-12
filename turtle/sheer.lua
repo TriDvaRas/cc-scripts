26b70951190593b99c1238d9e81a29bf84760185
TriDvaRas <02dimon02@gmail.com>
2022-07-12 18:04:55 +0300

    sheer

---
+++ turtle/sheer.lua
@@ -0,0 +1,27 @@
+local function dance()
+    turtle.turnLeft()
+    turtle.turnLeft()
+    turtle.turnLeft()
+    turtle.turnLeft()
+end
+
+local function main()
+    local speaker = peripheral.find("speaker")
+    while true do
+        turtle.placeDown()
+        turtle.suckDown()
+        turtle.dropUp()
+        if speaker then
+            if math.random()<0.5 then
+                speaker.playSound("minecraft:entity.pig.ambient",1.0)
+            elseif math.random()<0.07 then
+                speaker.playSound("minecraft:entity.generic.explode",2.0)
+            elseif math.random()<0.1 then
+                speaker.playSound("minecraft:entity.ender_dragon.death",3.0)
+            end
+        end
+        sleep(10)
+    end
+end
+
+main()
